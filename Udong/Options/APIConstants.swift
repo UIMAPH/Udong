6afb4ad37b1167d90c49063920e23f5211d3b386
pccommen <pccommen@gmail.com>
2022-03-03 21:58:47 +0900

    first step

---
+++ Udong/Options/APIConstants.swift
@@ -0,0 +1,8 @@
+//
+//  APIConstants.swift
+//  Udong
+//
+//  Created by changgyo seo on 2022/03/03.
+//
+
+import Foundation
--- Udong/Resource/Login/First/FirstViewController.swift
+++ Udong/Resource/Login/First/FirstViewController.swift
@@ -56,5 +56,6 @@ class FirstViewController: UIViewController {
     
     func settingUI(){
         
+        
     }
 }
